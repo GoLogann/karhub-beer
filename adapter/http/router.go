00b2d7b1536834557cbdd3764c557e0206276dcb
Logan Cardoso <logan.cc@outlook.com>
2025-08-25 23:40:56 -0300

    feat: adiciona o módulo de roteamento e inicialização do servidor HTTP

---
+++ adapter/http/router.go
@@ -0,0 +1,48 @@
+package http
+
+import (
+	"context"
+
+	"github.com/GoLogann/karhub-beer/adapter/http/handler"
+	"github.com/GoLogann/karhub-beer/adapter/http/router"
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+	"go.uber.org/fx"
+)
+
+func StartServer(lc fx.Lifecycle, r *gin.Engine) {
+	port := ":8080"
+	lc.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			go func() {
+				logrus.Infof("API rodando na porta %s", port)
+				if err := r.Run(port); err != nil {
+					logrus.Errorf("Erro ao iniciar o servidor: %v", err)
+				}
+			}()
+			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			logrus.Info("API foi finalizada")
+			return nil
+		},
+	})
+}
+
+func RegisterRoutes(
+	r *gin.Engine,
+	beerHandler *handler.BeerHandler,
+) {
+	router.SetupBeerRoutes(r, beerHandler)
+}
+
+func SetupRouter() *gin.Engine {
+	return gin.Default()
+}
+
+func RouterModule() fx.Option {
+	return fx.Options(
+		fx.Provide(SetupRouter),
+		fx.Invoke(StartServer),
+	)
+}
