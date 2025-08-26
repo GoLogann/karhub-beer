64f448974d387bdc282e205fb9940897b7fe4c08
Logan Cardoso <logan.cc@outlook.com>
2025-08-26 15:23:36 -0300

    fix: corrige a configuração das rotas de cervejas para usar o middleware de autenticação

--- adapter/http/router/beer_router.go
+++ adapter/http/router/beer_router.go
@@ -3,17 +3,18 @@ package router
 import (
 	"github.com/GoLogann/karhub-beer/adapter/http/handler"
 	"github.com/gin-gonic/gin"
+	"github.com/GoLogann/karhub-beer/infra/auth"
 )
 
-func SetupBeerRoutes(r *gin.Engine, h *handler.BeerHandler) {
-	group := r.Group("/api/v1/beers")
+func SetupBeerRoutes(r *gin.RouterGroup, h *handler.BeerHandler) {
+	group := r.Group("/beers")
 	{
-		group.POST("/", h.Create)
 		group.GET("/", h.GetAll)
 		group.GET("/:id", h.GetByID)
-		group.PUT("/:id", h.Update)
-		group.DELETE("/:id", h.Delete)
-
 		group.POST("/recommend", h.Recommend)
+
+		group.POST("/", auth.RoleAuthMiddleware("admin"), h.Create)
+		group.PUT("/:id", auth.RoleAuthMiddleware("admin"), h.Update)
+		group.DELETE("/:id", auth.RoleAuthMiddleware("admin"), h.Delete)
 	}
 }
