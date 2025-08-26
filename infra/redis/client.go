f74a1e6394a6d60954eadf99959f4ad64cd42128
Logan Cardoso <logan.cc@outlook.com>
2025-08-26 15:48:56 -0300

    feat: add Redis client with basic Get/Set and TTL support

---
+++ infra/redis/client.go
@@ -0,0 +1,34 @@
+package redis
+
+import (
+	"context"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+type Client struct {
+	rdb *redis.Client
+}
+
+func NewClient(addr, password string, db int) *Client {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     addr,     
+		Password: password, 
+		DB:       db,
+	})
+
+	return &Client{rdb: rdb}
+}
+
+func (c *Client) Set(ctx context.Context, key string, value string, ttl time.Duration) error {
+	return c.rdb.Set(ctx, key, value, ttl).Err()
+}
+
+func (c *Client) Get(ctx context.Context, key string) (string, error) {
+	return c.rdb.Get(ctx, key).Result()
+}
+
+func (c *Client) Close() error {
+	return c.rdb.Close()
+}
