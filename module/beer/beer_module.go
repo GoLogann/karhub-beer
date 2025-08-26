058896c30a97938fe7932e6c467d35c979ff256e
Logan Cardoso <logan.cc@outlook.com>
2025-08-25 23:41:50 -0300

    feat: adiciona módulos de gerenciamento de cervejas e integração com Spotify

---
+++ module/beer/beer_module.go
@@ -0,0 +1,28 @@
+package beer
+
+import (
+	"github.com/GoLogann/karhub-beer/adapter/http/handler"
+	"github.com/GoLogann/karhub-beer/adapter/repository"
+	"github.com/GoLogann/karhub-beer/core/usecase"
+	"github.com/GoLogann/karhub-beer/infra/spotify"
+	"go.uber.org/fx"
+	"gorm.io/gorm"
+)
+
+var BeerModule = fx.Options(
+	fx.Provide(NewBeerRepository),
+	fx.Provide(NewBeerUseCase),
+	fx.Provide(NewBeerHandler),
+)
+
+func NewBeerRepository(db *gorm.DB) repository.BeerRepository {
+	return repository.NewBeerRepository(db)
+}
+
+func NewBeerUseCase(repo repository.BeerRepository) *usecase.BeerUseCase {
+	return usecase.NewBeerUseCase(repo)
+}
+
+func NewBeerHandler(uc *usecase.BeerUseCase, sc *spotify.Client) *handler.BeerHandler {
+	return handler.NewBeerHandler(uc, sc)
+}
---
+++ module/spotify/spotify_module.go
@@ -0,0 +1,10 @@
+package spotify
+
+import (
+	"github.com/GoLogann/karhub-beer/infra/spotify"
+	"go.uber.org/fx"
+)
+
+var SpotifyModule = fx.Options(
+	fx.Provide(spotify.NewClient),
+)
