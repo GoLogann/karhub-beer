0c3b903c7d5aa1c1972c68ed46c0bf284f25c16f
Logan Cardoso <logan.cc@outlook.com>
2025-08-30 16:41:10 -0300

    fix: add script para criada o db para o keycloak

--- initdb.sh
+++ initdb.sh
@@ -3,5 +3,8 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE DATABASE karhub_beer;
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE DATABASE keycloak;
 EOSQL
